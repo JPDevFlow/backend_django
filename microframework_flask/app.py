f7418179f9eb5e230981f502c778f5cf6791105c
JEHYP1021 <jehypo1021@gmail.com>
2025-05-14 10:54:31 +0200

    inclusion de html con bootstrap

--- microframework_flask/app.py
+++ microframework_flask/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/') #url: http://localhost:5000/
+@app.route("/index.html") #url: http://localhost:5000/index.html
 def inicio():
     app.logger.debug("Entramos al path de inicio/")
-    return "<p>Hola Mundo desde Flask</p>"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
